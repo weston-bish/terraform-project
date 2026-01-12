79f554ff8ef648de73fd9810e92b8bed0a94851e
Weston Bishop <westonbishop366@gmail.com>
2026-01-11 21:06:44 -0500

    Add module VPC

--- main.tf
+++ main.tf
@@ -25,9 +25,26 @@ resource "aws_instance" "app_server" {
   // aws free tier
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id = module.vpc.private_subnets[0];
+
   // EC2 instance tag arguments
   tags = {
     Name = var.instance_name
   }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames    = true
 }
 
